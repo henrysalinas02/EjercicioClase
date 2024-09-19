65bd7b6a20ab51200de7b888fcf363d67e0eadbd
henrysalinas02 <henrysalinas557@gmail.com>
2024-09-18 23:36:58 -0600

    Se agrego una nueva opcion para sumar un tercer numero

--- operacion.php
+++ operacion.php
@@ -2,10 +2,11 @@
     // Obtener los valores de los campos del formulario
     $num1 = $_POST["num1"];
     $num2 = $_POST["num2"];
+    $num3 = $_POST["num3"];
 
     // Realizar la suma
-    $suma = $num1 + $num2;
+    $suma = $num1 + $num2 + $num3;
 
     // Mostrar el resultado
-    echo "La suma de $num1 + $num2 es: " . $suma;
+    echo "La suma de $num1 + $num2 + $num3 es: " . $suma;
 ?>
